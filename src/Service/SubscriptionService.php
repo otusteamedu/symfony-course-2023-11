otusteamedu/symfony-course-2023-02

Add feed and notifications

src/Service/SubscriptionService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\DTO\AddFollowersDTO;
 use App\DTO\ManageUserDTO;
+use App\Entity\Subscription;
 use App\Entity\User;
 use App\Manager\SubscriptionManager;
 use App\Manager\UserManager;
@@ -38,9 +39,20 @@ class SubscriptionService
     {
         $createdFollowers = 0;
         for ($i = 0; $i < $count; $i++) {
+            $login = "{$followerLogin}_#$i";
             $followerId = $this->userManager->saveUserFromDTO(
                 new User(),
-                new ManageUserDTO("{$followerLogin}_#$i", $followerLogin, $i, true)
+                new ManageUserDTO(
+                    $login,
+                    $followerLogin,
+                    $i,
+                    true,
+                    [],
+                    [],
+                    '+'.str_pad((string)abs(crc32($login)), 10, '0'),
+                    "$login@gmail.com",
+                    random_int(0, 1) === 1 ? User::EMAIL_NOTIFICATION : User::SMS_NOTIFICATION,
+                )
             );
             if ($followerId !== null) {
                 $this->subscribe($user->getId(), $followerId);
@@ -64,5 +76,30 @@ class SubscriptionService
         }
 
         return $result;
+    }
+
+    /**
+     * @return int[]
+     */
+    public function getFollowerIds(int $authorId): array
+    {
+        $subscriptions = $this->getSubscriptionsByAuthorId($authorId);
+        $mapper = static function(Subscription $subscription) {
+            return $subscription->getFollower()->getId();
+        };
+
+        return array_map($mapper, $subscriptions);
+    }
+
+    /**
+     * @return Subscription[]
+     */
+    private function getSubscriptionsByAuthorId(int $authorId): array
+    {
+        $author = $this->userManager->getUserById($authorId);
+        if (!($author instanceof User)) {
+            return [];
+        }
+        return $this->subscriptionManager->findAllByAuthor($author);
     }
 }

otusteamedu/symfony-course-2023-11

src/Service/SubscriptionService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\DTO\AddFollowersDTO;
 use App\DTO\ManageUserDTO;
+use App\Entity\Subscription;
 use App\Entity\User;
 use App\Manager\SubscriptionManager;
 use App\Manager\UserManager;
@@ -38,9 +39,20 @@ class SubscriptionService
     {
         $createdFollowers = 0;
         for ($i = 0; $i < $count; $i++) {
+            $login = "{$followerLogin}_#$i";
             $followerId = $this->userManager->saveUserFromDTO(
                 new User(),
-                new ManageUserDTO("{$followerLogin}_#$i", $followerLogin, $i, true)
+                new ManageUserDTO(
+                    $login,
+                    $followerLogin,
+                    $i,
+                    true,
+                    [],
+                    [],
+                    '+'.str_pad((string)abs(crc32($login)), 10, '0'),
+                    "$login@gmail.com",
+                    random_int(0, 1) === 1 ? User::EMAIL_NOTIFICATION : User::SMS_NOTIFICATION,
+                )
             );
             if ($followerId !== null) {
                 $this->subscribe($user->getId(), $followerId);
@@ -64,5 +76,30 @@ class SubscriptionService
         }
 
         return $result;
+    }
+
+    /**
+     * @return int[]
+     */
+    public function getFollowerIds(int $authorId): array
+    {
+        $subscriptions = $this->getSubscriptionsByAuthorId($authorId);
+        $mapper = static function(Subscription $subscription) {
+            return $subscription->getFollower()->getId();
+        };
+
+        return array_map($mapper, $subscriptions);
+    }
+
+    /**
+     * @return Subscription[]
+     */
+    private function getSubscriptionsByAuthorId(int $authorId): array
+    {
+        $author = $this->userManager->findUser($authorId);
+        if (!($author instanceof User)) {
+            return [];
+        }
+        return $this->subscriptionManager->findAllByAuthor($author);
     }
 }